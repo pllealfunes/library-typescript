470ecd21bc9bfa2558adb61ea15a3a461c288287
Paula Leal <paula61395@gmail.com>
2023-07-30 23:03:51 -0400

    able to change read button status

--- library-typescript/src/main.ts
+++ library-typescript/src/main.ts
@@ -19,36 +19,53 @@ class newBook implements Book {
     ) { }
 }
 
+const books: object[] = [];
+
 const newBookBtn = document.getElementById("newBookForm") as HTMLFormElement
 
 newBookBtn.addEventListener("submit", (event: SubmitEvent): void => {
     event.preventDefault()
 
-    // Get the new item value
+    // Get the new book values from form
     const bookTitle = (document.getElementById("newBookTitle") as HTMLInputElement).value
     const bookAuthor = (document.getElementById("newBookAuthor") as HTMLInputElement).value
     const pageCount = (document.getElementById("pageCount") as HTMLInputElement).value
     const ifRead = (document.getElementById("ifRead") as HTMLInputElement).checked
+    const bookId = books.length ? books.length + 1 : 1
 
-    const createBook = new newBook("1", bookTitle, bookAuthor, pageCount, ifRead)
-    console.log(createBook)
+    // Create new book object
+    const createBook = new newBook(String(bookId), bookTitle, bookAuthor, pageCount, ifRead)
 
     const shelf = document.getElementById("shelf") as HTMLFormElement
 
+    // Create book details li
+
     const bookContainer = document.createElement("li");
     const deleteContainer = document.createElement("div");
+
+
     const deleteBtn = document.createElement("button")
+
     const title = document.createElement("h3")
     const author = document.createElement("p")
     const count = document.createElement("p")
     const readStatus = document.createElement("button")
 
-    bookContainer.textContent = createBook.title
+    bookContainer.setAttribute("class", "book")
+
+
+    deleteBtn.setAttribute("class", "deleteBtn")
     deleteBtn.textContent = "X"
+
+
+    title.setAttribute("class", "bookTitle")
     title.textContent = createBook.title
-    author.textContent = createBook.author
-    count.textContent = createBook.pages
+    author.setAttribute("class", "bookAuthor")
+    author.textContent = `By: ${createBook.author}`
+    count.setAttribute("class", "pageCount")
+    count.textContent = `Pages: ${createBook.pages}`
     readStatus.textContent = createBook.checked ? "Read" : "Not Read"
+    readStatus.setAttribute("class", "readStatus")
 
     shelf?.appendChild(bookContainer);
     bookContainer?.appendChild(deleteContainer);
@@ -57,14 +74,25 @@ newBookBtn.addEventListener("submit", (event: SubmitEvent): void => {
     bookContainer?.appendChild(author);
     bookContainer?.appendChild(count);
     bookContainer?.appendChild(readStatus);
+
+    // Push new book to books array
+    books.push(newBook)
+    console.log(newBook)
+
+    // Add event listener on "read" book button
+    readStatus.addEventListener('click', () => {
+        const newStatus = createBook.checked ? createBook.checked = false : createBook.checked = true
+        newStatus ? readStatus.innerText = "Read" : readStatus.innerText = "Not Read"
+        console.log(books)
+    })
+
+    // Delete book from books array
+    deleteBtn.addEventListener('click', () => {
+        const array = books.filter(book => book != createBook)
+        console.log(array);
+
+
+    })
 })
 
 
-
-// <div class="deleteContainer">
-//             <button class="deleteBtn">X</button>
-//           </div>
-//           <h3 class="bookTitle">Book 1</h3>
-//           <p class="bookAuthor">By: Author 1</p>
-//           <p class="pageCount">Pages: 123</p>
-//           <button class="readStatus">Read</button>
