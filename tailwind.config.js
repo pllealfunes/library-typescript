e71386d98524e1dd33fc4170821be047a44db881
Paula Leal <paula61395@gmail.com>
2023-08-20 00:07:24 -0400

    edited tailwind and vite config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./dist/index.html",
-    "./dist/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 
 export default ({
-    base: "/library-typescript/"
+    base: "/library-typescript/",
+    build: {
+        outDir: 'dist',
+    },
 })
